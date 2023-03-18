fbab0293987281419aa274ba29e859c8d61b633c
Antoinette <philipinamensah19@gmail.com>
2023-03-17 18:00:53 -0700

    A program that prints out random numbers each time it's executed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive:\n", n);
+		printf("%d is positive\n", n);
 	}
 	if (n == 0)
 	{
@@ -22,7 +22,7 @@ int main(void)
 	}
 	if (n < 0)
 	{
-		printf("%d is negative:\n", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
