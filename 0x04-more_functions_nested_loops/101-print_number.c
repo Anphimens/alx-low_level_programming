77a6aa639454e6a748670e3858570aaa737f96b6
Antoinette <philipinamensah19@gmail.com>
2023-03-23 18:28:24 -0700

    Print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,14 +7,16 @@
  */
 void print_number(int n)
 {
+	unsigned int i = n;
+
 	if (n < 0)
 	{
 		_putchar(45);
-		n = -n;
+		i = -i;
 	}
-	if (n / 10)
+	if (i / 10)
 	{
-		print_number(n / 10);
+		print_number(i / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar(i % 10 + '0');
 }
