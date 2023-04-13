bc28cc4de55042db0cf02f6133021e576a0af72b
Antoinette <philipinamensah19@gmail.com>
2023-04-13 15:13:31 -0700

    My header file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,5 +6,7 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+int _putchar(char c);
+char **strtow(char *str);
 
 #endif
