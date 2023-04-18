f4c47c29d04567df4423d9f59ae65d3180a69634
Antoinette <philipinamensah19@gmail.com>
2023-04-17 19:41:50 -0700

    header file

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil)"));
-		printf("Age: %f\n", d->age);
+		d-> age ? (printf("Age: %f\n", d->age)) : (printf("Age: (nil)"));
 		d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)"));
 	}
 }
