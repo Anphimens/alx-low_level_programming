d950d14942d62e28ab03a481246dee436e80133e
Antoinette <philipinamensah19@gmail.com>
2023-04-17 17:26:11 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - My name is dog and i am defining myself
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Return: a call to the member in the struct
+ */
 struct dog
 {
 	char *name;
