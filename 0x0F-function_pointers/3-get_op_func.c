022a1cdd65458dcca4f130681085eaf3ad76e589
Antoinette <philipinamensah19@gmail.com>
2023-04-20 17:23:45 -0700

    A program that performs simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stddef.h>
+
+/**
+ * get_op_func - selects the correct function to perform the
+ *		operatin asked by the user
+ * @s: the operator passed as argument
+ * Return: a pointer to the function that matches with the operator given
+ *		given as argument
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*ops[i].op == *s && *(s + 1) == '\0')
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+
+/**
+ * op_add - operator function for adding two int
+ * @a: first int
+ * @b: second int
+ * Return: 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - operator to subtract two ints
+ * @a: first int
+ * @b: second int
+ * Return: 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - operator to multiply two int
+ * @a: first int
+ * @b: second int
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - operator to divide two int
+ * @a: first int
+ * @b: second int
+ * Return: 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - operator for finding the reminder of an int
+ * @a: first int
+ * @b: second int
+ * Return: 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
