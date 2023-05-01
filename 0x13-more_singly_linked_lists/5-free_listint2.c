d276323d709425d4c3c84ad53936521177d90258
Anphimens <philipinamensah19@gmail.com>
2023-04-30 21:02:25 -0700

    A function that frees a linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,16 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
-	
-	while (*head != NULL)
+	listint_t *temp, *current;
+
+	if (head == NULL || *head == NULL)
+		return;
+	current = *head;
+
+	while (current != NULL)
 	{
-		temp = *head;
-		*head = (*head)->next;
+		temp = current;
+		current = current->next;
 		free(temp);
 	}
 	*head = NULL;
