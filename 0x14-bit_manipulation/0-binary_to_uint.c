167f43b85c8c0e2666534ba8e1e6454c28d85b61
Anotinette Mensah <philipinamensah19@gmail.com>
2023-07-06 23:04:26 +0000

    This directory deleted because I needed to restart all over

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to a string of 0 and 1 chars
+ *
+ * Return: the converted number or 0
+ *		if one or more chars in the string b is not 0 or 1
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int i;
+
+	if (b == NULL)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+		result = result * 2 + (b[i] - '0');
+	}
+	return (result);
+}
