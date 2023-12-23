5a3fcbd2fd6b46c4c6db79fb6fa8ac0407621f88
Anphimens <philipinamensah19@gmail.com>
2023-12-23 01:53:48 +0000

    Creating hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -21,7 +21,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hashTable->array = malloc(sizeof(hash_node_t) * size);
 	if (hashTable->array == NULL)
 	{
-		free(hashTable);
 		return (NULL);
 	}
 	/*Initialize each array element to NULL */
